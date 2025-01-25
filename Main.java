b2cf7be6a98aee5f7715f8b2fc22193d11941e7e
AdrianCoradoe <adriancorado957@gmail.com>
2025-01-25 16:32:33 -0600

    Actu con main completo

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Licuadora miLicuadora = new LicuadoraSimple();
+        int opcion;
+
